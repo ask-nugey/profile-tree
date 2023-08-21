118c0754c0fde1c7994bafc21516cc15c6a15fc1
ask-nugey <mischief.moi@gmail.com>
2023-08-22 00:23:32 +0900

    🎨 Updated content displayed on the profile.

--- src/app/profile/Ansers.tsx
+++ src/app/profile/Ansers.tsx
@@ -1,18 +1,49 @@
 "use client"
 
-import useRetrieveAnswers from "@/app/profile/useRetrieveAnswers";
+import { useEffect } from "react";
 
-export default function Ansers() {
-  const answers = useRetrieveAnswers();
+export default function Ansers({answers, questions, setQuestions}) {
+  useEffect(() => {
+    const getData = async () => {
+      const res = await fetch(`/question/api/`, {
+        next: { revalidate: 60 },
+      });
+      const data = await res.json();
+
+      setQuestions(data);
+    };
+
+    getData();
+  }, []);
+
+  // questionId から prefix を取得するヘルパー関数
+  const getQuestionPrefix = (id: number): string | undefined => {
+    const question = questions?.find((q) => q.id === id);
+    return question?.prefix;
+  };
+
+  // value から対応する answerText を取得するヘルパー関数
+  const getAnswerText = (
+    questionId: number,
+    value: string
+  ): string | undefined => {
+    const question = questions?.find((q) => q.id === questionId);
+    const choice = question?.choices.find((c) => c.value === value);
+    return choice?.answerText;
+  };
 
   return (
-    <ul className="c-answerList">
+    <div className="c-answerList">
       {answers.map((answer, index) => (
-        <li className="c-answerItem" key={index}>
-          {answer?.questionId}
-          {answer?.values.map((value) => ( value ))}
-        </li>
+        <div className="c-answerItem" key={index}>
+          <p className="title">{getQuestionPrefix(answer?.questionId)}</p>
+          {answer?.values.map((value, valueIndex) => (
+            <p className="text" key={valueIndex}>
+              {getAnswerText(answer?.questionId, value)}
+            </p>
+          ))}
+        </div>
       ))}
-    </ul>
+    </div>
   );
 };
--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,12 +1,23 @@
+"use client";
+
 import Ansers from "@/app/profile/Ansers";
+import useRetrieveAnswers from "@/app/profile/useRetrieveAnswers";
+import { Question } from "@/types";
+import { useState } from "react";
 
 export default function Page() {
+  const answers = useRetrieveAnswers();
+  const [questions, setQuestions] = useState<Question[] | null>(null);
 
   return (
     <main>
-      <div className='c-block'>
+      <div className="c-block">
         <h1 className="c-answerTitle">あなたのプロフィール</h1>
-        <Ansers />
+        <Ansers
+          answers={answers}
+          questions={questions}
+          setQuestions={setQuestions}
+        />
       </div>
     </main>
   );
